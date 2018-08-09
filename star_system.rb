eebe6086711e34fd760755f4b86cb8a877527718
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-08-09 14:14:50 +0100

    Add corrections

--- star_system.rb
+++ star_system.rb
@@ -27,17 +27,20 @@ class StarSystem
   end
 
   def get_planets_with_more_moons(number)
-    planets = @planets.select { |planet| planet.number_of_moons > number }
-    planets.map { |planet| planet.name}
+    planets_with_more_moons = @planets.select { |planet| planet.number_of_moons > number }
+    planets_with_more_moons.map { |planet| planet.name}
   end
 
   def get_number_of_planets_closer_than(number)
-    planets = @planets.select { |planet| planet.distance_from_sun < 1000 }
-    planets.count
+    # planets_arr = @planets.select { |planet| planet.distance_from_sun < number }
+    # planets_arr.count
+
+    # Count takes blocks as arguments
+    @planets.count { |planet| planet.distance_from_sun < number }
   end
 
   def get_total_number_of_moons
-    @planets.reduce(0) {|total, planet| total + planet.number_of_moons}
+    @planets.reduce(0) { |total, planet| total + planet.number_of_moons }
   end
 
   def get_planet_names_sorted_by_increasing_distance_from_sun
