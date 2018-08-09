369907dd3141ea30070cd54419847f21a9557b1c
VladmirVolgen <alvaroalonsoflor@gmail.com>
2018-08-09 12:29:44 +0100

    Add numbers_of_planets_closer_than method

--- star_system.rb
+++ star_system.rb
@@ -31,4 +31,9 @@ class StarSystem
     planets.map { |planet| planet.name}
   end
 
+  def get_number_of_planets_closer_than(number)
+    planets = @planets.select { |planet| planet.distance_from_sun < 1000 }
+    planets.count
+  end
+
 end
